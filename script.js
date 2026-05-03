8d926ca748c9f9d839e7c01c360169777adb5a58
Bilon-22 <dulalbilon@stu.ac.kr>
2026-05-03 16:31:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Ask user name
+let userName = prompt("What is your name?");
+
+// Get current hour
+let hour = new Date().getHours();
+
+// Greeting
+if (hour < 12) {
+  console.log("Good morning, " + userName + "!");
+} else if (hour < 18) {
+  console.log("Good afternoon, " + userName + "!");
+} else {
+  console.log("Good evening, " + userName + "!");
+}
+
+// Ask birth year
+let birthYear = prompt("What year were you born?");
+
+// Calculate age
+let age = 2026 - birthYear;
+
+// Calculate days lived
+let daysLived = age * 365;
+
+console.log("You have lived for approximately " + daysLived + " days!");
+
+// Adult check
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
